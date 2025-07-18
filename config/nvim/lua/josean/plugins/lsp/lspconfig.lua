6a76b346633ce40c4f7c5c06944b0e8690e5c484
Karol Rungo <karolrungo@gmail.com>
2025-07-18 11:54:36 +0200

    mason lsp confiog breaking change fix

--- config/nvim/lua/josean/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/josean/plugins/lsp/lspconfig.lua
@@ -77,52 +77,54 @@ return {
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["tsp_server"] = function()
-				-- configure emmet language server
-				lspconfig["tsp_server"].setup({
-					capabilities = capabilities,
-					filetypes = { "typescript", "javascript" },
-				})
-			end,
-			["gopls"] = function()
-				-- configure go language server
-				lspconfig["gopls"].setup({
-					capabilities = capabilities,
-					filetypes = { "go" },
-				})
-			end,
-			["clangd"] = function()
-				-- configure cpp language server
-				lspconfig["clangd"].setup({
-					cmd = { "/usr/bin/clangd" },
-					capabilities = capabilities,
-					filetypes = { "cpp", "c" },
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
+		mason_lspconfig.setup({
+			handlers = {
+				-- default handler for installed servers
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				["tsp_server"] = function()
+					-- configure emmet language server
+					lspconfig["tsp_server"].setup({
+						capabilities = capabilities,
+						filetypes = { "typescript", "javascript" },
+					})
+				end,
+				["gopls"] = function()
+					-- configure go language server
+					lspconfig["gopls"].setup({
+						capabilities = capabilities,
+						filetypes = { "go" },
+					})
+				end,
+				["clangd"] = function()
+					-- configure cpp language server
+					lspconfig["clangd"].setup({
+						cmd = { "/usr/bin/clangd" },
+						capabilities = capabilities,
+						filetypes = { "cpp", "c" },
+					})
+				end,
+				["lua_ls"] = function()
+					-- configure lua server (with special settings)
+					lspconfig["lua_ls"].setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								-- make the language server recognize "vim" global
+								diagnostics = {
+									globals = { "vim" },
+								},
+								completion = {
+									callSnippet = "Replace",
+								},
 							},
 						},
-					},
-				})
-			end,
+					})
+				end,
+			},
 		})
 	end,
 }
